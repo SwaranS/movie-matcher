2ff5a396bab8b56b78a92cb4b5c0ca7862befa9e
Swaran <singhsw@tcd.ie>
2021-01-30 22:12:30 +0000

    First Commit for Movie Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from "./components/card-list/card-list.componenets";
+import { SearchBox } from "./components/search-box/search-box.component";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      locals: [],
+      searchField: " ",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ locals: users }));
+  }
+  render() {
+    const { locals, searchField } = this.state;
+    const filteredUsers = locals.filter((localUser) =>
+      localUser.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1>Search Users</h1>
+        <SearchBox
+        placeholder = 'Search Users' 
+        handleChange = {e => this.setState({searchField : e.target.value})}/>
+        
+        <CardList users={filteredUsers}></CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
